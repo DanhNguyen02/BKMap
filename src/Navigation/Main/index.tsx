356c8cbb7b7728b8db9cf513826a2c81a611a491
tona3922 <vothaitoan12@gmail.com>
2023-12-14 21:17:39 +0700

    feat: add navigation

--- src/Navigation/Main/index.tsx
+++ src/Navigation/Main/index.tsx
@@ -1,21 +1,85 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeContainer } from "@/Screens/Home";
-
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Button, View } from "native-base";
+import Map from "@/Screens/Map/Map";
+import Explore from "@/Screens/Explore/Explore";
+import Profile from "@/Screens/Profile/Profile";
+import QRScan from "@/Screens/QRScan/QRScan";
 const Tab = createBottomTabNavigator();
 
 // @refresh reset
 export const MainNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Location") {
+            iconName = focused ? "location" : "location-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          } else {
+            iconName = focused ? "compass" : "compass-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={35} color={color} />;
+        },
+        tabBarActiveTintColor: "#22668D",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+        tabBarStyle: {
+          height: 60,
+          borderTopLeftRadius: 16,
+          borderTopRightRadius: 16,
+          position: "absolute",
+        },
+        tabBarShowLabel: false,
+        // tabBarLabelStyle: {
+        //   fontSize: 16,
+        // },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeContainer} />
+      <Tab.Screen name="Explore" component={Explore} />
       <Tab.Screen
-        name="Home"
-        component={HomeContainer}
+        name="Add"
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabel: "",
+          tabBarIcon: ({}) => (
+            <View
+              backgroundColor={"#22668D"}
+              w="16"
+              h="16"
+              bottom={4}
+              borderRadius={100}
+              display={"flex"}
+              justifyContent={"center"}
+              p="3.5"
+              // alignItems={"}
+              // style={{
+              //   backgroundColor: "#22668D",
+              //   bottom: 20,
+              //   borderRadius: 100,
+              // }}
+            >
+              <MaterialCommunityIcons
+                name="qrcode-scan"
+                size={35}
+                color="white"
+              />
+            </View>
+          ),
         }}
+        component={QRScan}
       />
+      <Tab.Screen name="Location" component={Map} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 };
